b5a8139dcf63c07a1627d024fc021beac4309dbd
Epitomaniac <112977644+Epitomaniac@users.noreply.github.com>
2023-05-17 07:25:35 +0330

    Add files via upload

--- components/CheckmatesNav.js
+++ components/CheckmatesNav.js
@@ -18,63 +18,65 @@ export default function CheckmatesNav({
         <li className="navbar-title">Checkmates</li>
         <li
           style={highlight1 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/1")}
+          onClick={() => router.push("/lessons/basic-checkmates/back-rank/1")}
           className="navbar-lesson"
         >
           Back Rank
         </li>
         <li
           style={highlight2 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/2")}
+          onClick={() => router.push("/lessons/basic-checkmates/battery/1")}
           className="navbar-lesson"
         >
           Battery
         </li>
         <li
           style={highlight3 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/3")}
+          onClick={() =>
+            router.push("/lessons/basic-checkmates/mate-exercise/1")
+          }
           className="navbar-lesson"
         >
           Exercise: Mate in 1
         </li>
         <li
           style={highlight4 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/4")}
+          onClick={() => router.push("/lessons/basic-checkmates/rook-queen/1")}
           className="navbar-lesson"
         >
           Queen & Rook vs King
         </li>
         <li
           style={highlight5 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/5")}
+          onClick={() => router.push("/lessons/basic-checkmates/two-rooks/1")}
           className="navbar-lesson"
         >
           Two Rooks vs King
         </li>
         <li
           style={highlight6 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/6")}
+          onClick={() => router.push("/lessons/basic-checkmates/king-queen/1")}
           className="navbar-lesson"
         >
           King & Queen vs King
         </li>
         <li
           style={highlight7 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/7")}
+          onClick={() => router.push("/lessons/basic-checkmates/exercise-1/1")}
           className="navbar-lesson"
         >
           Exercise: Q + R vs K
         </li>
         <li
           style={highlight8 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/8")}
+          onClick={() => router.push("/lessons/basic-checkmates/exercise-2/1")}
           className="navbar-lesson"
         >
           Exercise: R + R vs K
         </li>
         <li
           style={highlight9 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-checkmates/9")}
+          onClick={() => router.push("/lessons/basic-checkmates/exercise-3/1")}
           className="navbar-lesson"
         >
           Exercise: K + Q vs K
--- components/TacticsNav.js
+++ components/TacticsNav.js
@@ -16,49 +16,53 @@ export default function TacticsNav({
         <li className="navbar-title">Tactics</li>
         <li
           style={highlight1 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/1")}
+          onClick={() => router.push("/lessons/basic-tactics/fork/1")}
           className="navbar-lesson"
         >
           The Fork
         </li>
         <li
           style={highlight2 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/2")}
+          onClick={() => router.push("/lessons/basic-tactics/pin/1")}
           className="navbar-lesson"
         >
           The Pin
         </li>
         <li
           style={highlight3 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/3")}
+          onClick={() => router.push("/lessons/basic-tactics/skewer/1")}
           className="navbar-lesson"
         >
           The Skewer
         </li>
         <li
           style={highlight4 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/4")}
+          onClick={() =>
+            router.push("/lessons/basic-tactics/discoverd-attack/1")
+          }
           className="navbar-lesson"
         >
           Discovered Attack
         </li>
         <li
           style={highlight5 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/5")}
+          onClick={() =>
+            router.push("/lessons/basic-tactics/removing-the-defender/1")
+          }
           className="navbar-lesson"
         >
           Removing the Defender
         </li>
         <li
           style={highlight6 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/6")}
+          onClick={() => router.push("/lessons/basic-tactics/sacrifice/1")}
           className="navbar-lesson"
         >
           Sacrifice
         </li>
         <li
           style={highlight7 && { backgroundColor: "#201f1f", color: "white" }}
-          onClick={() => router.push("/lessons/basic-tactics/7")}
+          onClick={() => router.push("/lessons/basic-tactics/exercise/1")}
           className="navbar-lesson"
         >
           Exercise: Tactics
